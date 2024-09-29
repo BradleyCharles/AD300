a25f7b93d89d4f7581ab7375e88a30ae963760f5
BradleyCharles <67434992+BradleyCharles@users.noreply.github.com>
2024-09-28 18:47:58 -0700

    Changed if else to switch case

--- Week1/week1-GPA-to-Percent-range/src/TestOptionPane03.java
+++ Week1/week1-GPA-to-Percent-range/src/TestOptionPane03.java
@@ -5,80 +5,47 @@ import java.lang.reflect.InvocationTargetException;
 public class TestOptionPane03 {
 
     public static void main(String[] args) {
-        String choice = ask("100 % to 95.0%", "<95.0 % to 94.0%", "<94.0 % to 93.0%", "<93.0 to 92.0%", "<92.0 % to 91.0%", "<91.0 % to 90.0%", "<90.0 % to 89.0%", "<89.0 % to 88.0%", "<88.0 % to 87.0%", "<87.0 % to 86.0%", "<86.0 % to 85.0%", "<85.0 % to 84.0%", "<84.0 % to 83.0%", "<83.0 % to 82.0%", "<82.0 % to 81.0%", "<81.0 % to 80.0%", "<80.0 % to 79.0%", "<79.0 % to 78.0%", "<78.0 % to 77.0%", "<77.0 % to 76.0%", "<76.0% to 75.0%", "<75.0% to 74.0%", "<74.0% to 73.0%", "<73.0% to 72.0%", "<72.0% to 71.0%", "<71.0% to 70.0%", "<70.0% to 69.0%", "<69.0% to 68.0%", "<68.0% to 67.0%", "<67.0% to 66.0%", "<66.0% to 65.0%", "<65.0% to 0.0%");
-        String result = "Null";
-
-        if (choice.equals("100 % to 95.0%")) {
-            result = "4.0";
-        } else if (choice.equals("<95.0% to 94.0%")) {
-            result = "3.9";
-        } else if (choice.equals("<94.0% to 93.0%")) {
-            result = "3.8";
-        } else if (choice.equals("<93.0% to 92.0%")) {
-            result = "3.7";
-        } else if (choice.equals("<92.0 % to 91.0%")) {
-            result = "3.6";
-        } else if (choice.equals("<91.0 % to 90.0%")) {
-            result = "3.5";
-        } else if (choice.equals("<90.0 % to 89.0%")) {
-            result = "3.4";
-        } else if (choice.equals("<89.0 % to 88.0%")) {
-            result = "3.3";
-        } else if (choice.equals("<88.0 % to 87.0%")) {
-            result = "3.2";
-        } else if (choice.equals("<87.0 % to 86.0%")) {
-            result = "3.1";
-        } else if (choice.equals("<86.0 % to 85.0%")) {
-            result = "3.0";
-        } else if (choice.equals("<85.0 % to 84.0%")) {
-            result = "2.9";
-        } else if (choice.equals("<84.0 % to 83.0%")) {
-            result = "2.8";
-        } else if (choice.equals("<83.0% to 82.0%")) {
-            result = "2.7";
-        } else if (choice.equals("<82.0% to 81.0%")) {
-            result = "2.6";
-        } else if (choice.equals("<81.0% to 80.0%")) {
-            result = "2.5";
-        } else if (choice.equals("<80.0% to 79.0%")) {
-            result = "2.4";
-        } else if (choice.equals("<79.0% to 78.0%")) {
-            result = "2.3";
-        } else if (choice.equals("<78.0% to 77.0%")) {
-            result = "2.2";
-        } else if (choice.equals("<77.0% to 76.0%")) {
-            result = "2.1";
-        } else if (choice.equals("<76.0% to 75.0%")) {
-            result = "2.0";
-        } else if (choice.equals("<75.0% to 74.0%")) {
-            result = "1.9";
-        } else if (choice.equals("<74.0% to 73.0%")) {
-            result = "1.8";
-        } else if (choice.equals("<73.0% to 72.0%")) {
-            result = "1.7";
-        } else if (choice.equals("<72.0% to 71.0%")) {
-            result = "1.6";
-        } else if (choice.equals("<71.0% to 70.0%")) {
-            result = "1.5";
-        } else if (choice.equals("<70.0% to 69.0%")) {
-            result = "1.4";
-        } else if (choice.equals("<69.0% to 68.0%")) {
-            result = "1.3";
-        } else if (choice.equals("<68.0% to 67.0%")) {
-            result = "1.2";
-        } else if (choice.equals("<67.0% to 66.0%")) {
-            result = "1.1";
-        } else if (choice.equals("<66.0% to 65.0%")) {
-            result = "1.0";
-        } else if (choice.equals("<65.0% to 0.0%")) {
-            result = "0.0";
-        }
+        String choice = ask("4.0", "3.9", "3.8", "3.7", "3.6", "3.5", "3.4", "3.3", "3.2", "3.1", "3.0", "2.9", "2.8", "2.7", "2.6", "2.5", "2.4", "2.3", "2.2", "2.1", "2.0", "1.9", "1.8", "1.7", "1.6", "1.5", "1.4", "1.3", "1.2", "1.1", "1.0", "0.0");
+        String result = switch (choice) {
+            case "4.0" -> "100 % to 95.0%";
+            case "3.9" -> "<95.0% to 94.0%";
+            case "3.8" -> "<94.0% to 93.0%";
+            case "3.7" -> "<93.0% to 92.0%";
+            case "3.6" -> "<92.0% to 91.0%";
+            case "3.5" -> "<91.0% to 90.0%";
+            case "3.4" -> "<90.0% to 89.0%";
+            case "3.3" -> "<89.0% to 88.0%";
+            case "3.2" -> "<88.0% to 87.0%";
+            case "3.1" -> "<87.0% to 86.0%";
+            case "3.0" -> "<86.0% to 85.0%";
+            case "2.9" -> "<85.0% to 84.0%";
+            case "2.8" -> "<84.0% to 83.0%";
+            case "2.7" -> "<83.0% to 82.0%";
+            case "2.6" -> "<82.0% to 81.0%";
+            case "2.5" -> "<81.0% to 80.0%";
+            case "2.4" -> "<80.0% to 79.0%";
+            case "2.3" -> "<79.0% to 78.0%";
+            case "2.2" -> "<78.0% to 77.0%";
+            case "2.1" -> "<77.0% to 76.0%";
+            case "2.0" -> "<76.0% to 75.0%";
+            case "1.9" -> "<75.0% to 74.0%";
+            case "1.8" -> "<74.0% to 73.0%";
+            case "1.7" -> "<73.0% to 72.0%";
+            case "1.6" -> "<72.0% to 71.0%";
+            case "1.5" -> "<71.0% to 70.0%";
+            case "1.4" -> "<70.0% to 69.0%";
+            case "1.3" -> "<69.0% to 68.0%";
+            case "1.2" -> "<68.0% to 67.0%";
+            case "1.1" -> "<67.0% to 66.0%";
+            case "1.0" -> "<66.0% to 0.0%";
+            default -> "0.0%";
+        };
 
 
 
-            JFrame frame = new JFrame("Main Window");
+        JFrame frame = new JFrame("Main Window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JOptionPane.showMessageDialog(frame, "You have a GPA of " + result);
+        JOptionPane.showMessageDialog(frame, "Your percent range is: " + result);
         System.exit(0);
 
 
