055dc081da6a5a3b0b378fb71e7312a0e298f2cf
Bradley Charles <bradgcharles@gmail.com>
2024-10-21 16:24:45 -0700

    More!

---
+++ Week4/Media/src/AudioPlayer.java
@@ -0,0 +1,12 @@
+public class AudioPlayer implements MediaPlayer {
+    
+    public String play() {
+        return "Audio is playing";
+    }
+    public String pause() {
+        return "Audio is paused";
+    }
+    public String stop() {
+        return "Audio is stopped";
+    }
+}
--- Week4/Media/src/Main.java
+++ Week4/Media/src/Main.java
@@ -1,5 +1,62 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    // Constructor for the main frame
+    public Main() {
+        setTitle("Animals.. am I right??");
+        setSize(750, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(3, 1, 10, 10)); // # buttons, # columns, spacing
+
+        Elephant elephant = new Elephant("Dumbo");
+
+        // Create buttons and add them to the panel
+        {
+            JButton button = new JButton("What does the elephant sound like?");
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    elephant.makeSound();
+                }
+            });
+            buttonPanel.add(button);
+        }
+        {
+            JButton button = new JButton("What is the sound a elephant makes called? (3 times)");
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    textArea.setText(
+                            elephant.getName() + " the elephant trumpets " +
+                     elephant.makeSound(2) + "!");
+                }
+
+            });
+            buttonPanel.add(button);
+        }
+        
+
+        add(buttonPanel, BorderLayout.EAST);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
+    }
+
+
 }
